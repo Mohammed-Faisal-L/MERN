b82721cdcfa9149cd07d1269a3661ab49aa15e65
Mohammed Faisal L <mdfaisal.a334@gmail.com>
2025-01-30 11:38:05 +0530

    path changed from js to javascript folder

--- javascript/domDisplay.js
+++ javascript/domDisplay.js
@@ -1,29 +1,47 @@
-const myApi = async () => {
-  await fetch("https://dummyjson.com/products")
-    .then(async (resp) => {
-      let response = await resp.json();
-      let products = response.products;
-      products.map((ele) => {
+  const myApi = async () => {
+    await fetch("https://dummyjson.com/products")
+      .then(async (resp) => {
+        let response = await resp.json();
+        let products = response.products;
+
+        let [id, title, description] = Object.keys(products[0]);
+
         let table = document.getElementsByTagName("table");
         let tableRow = document.createElement("tr");
 
-        let td1 = document.createElement("td");
-        let td2 = document.createElement("td");
-        let td3 = document.createElement("td");
+        let th1 = document.createElement("th");
+        let th2 = document.createElement("th");
+        let th3 = document.createElement("th");
 
-        td1.innerText = ele.id;
-        td2.innerText = ele.title;
-        td3.innerText = ele.description;
+        let capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-        tableRow.appendChild(td1);
-        tableRow.appendChild(td2);
-        tableRow.appendChild(td3);
+        th1.innerText = capitalize(id);
+        th2.innerText = capitalize(title);
+        th3.innerText = capitalize(description);
+
+        tableRow.append(th1, th2, th3);
 
         table[0].appendChild(tableRow);
+
+        products.map((ele) => {
+          table = document.getElementsByTagName("table");
+          tableRow = document.createElement("tr");
+
+          let td1 = document.createElement("td");
+          let td2 = document.createElement("td");
+          let td3 = document.createElement("td");
+
+          td1.innerText = ele.id;
+          td2.innerText = ele.title;
+          td3.innerText = ele.description;
+
+          tableRow.append(td1, td2, td3);
+
+          table[0].appendChild(tableRow);
+        });
+      })
+      .catch((msg) => {
+        console.log(msg);
       });
-    })
-    .catch((msg) => {
-      console.log(msg);
-    });
-};
-myApi();
+  };
+  myApi();
